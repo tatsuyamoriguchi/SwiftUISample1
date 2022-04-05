6f6eae9d1cb88372fc727be48145a0903a002f18
Brian Moriguchi <brian@beckos.com>
2022-04-05 11:28:43 -0700

    Initial Commit

---
+++ SwiftUISample1/SwiftUISample1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISample1App.swift
+//  SwiftUISample1
+//
+//  Created by Tatsuya Moriguchi on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
